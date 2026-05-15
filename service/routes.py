fc91528ef04bf5363682efb475505998ace6d944
Christian Pesch <pu5ch3r@gmail.com>
2026-05-15 03:33:16 -0400

    fix whitespaces

--- service/routes.py
+++ service/routes.py
@@ -60,7 +60,6 @@ def create_accounts():
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
-
 @app.route("/accounts", methods=["GET"])
 def read_all_accounts():
     """
@@ -75,7 +74,6 @@ def read_all_accounts():
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
-
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def read_account(account_id):
     """
@@ -88,13 +86,12 @@ def read_account(account_id):
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
 
-    return account.serialize(), status.HTTP_200_OK    
+    return account.serialize(), status.HTTP_200_OK
 
 
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
-
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_account(account_id):
     """
@@ -110,14 +107,13 @@ def update_account(account_id):
     account.deserialize(request.get_json())
     account.update()
 
-    return account.serialize(), status.HTTP_200_OK    
+    return account.serialize(), status.HTTP_200_OK
 
 
 
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
-
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_account(account_id):
     """
@@ -130,7 +126,7 @@ def delete_account(account_id):
     if account:
         account.delete()
 
-    return {}, status.HTTP_204_NO_CONTENT   
+    return {}, status.HTTP_204_NO_CONTENT
 
 
 
